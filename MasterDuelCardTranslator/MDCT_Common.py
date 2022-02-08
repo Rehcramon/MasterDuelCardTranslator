f0799d63ea8bca43363bca4035edea03fc36f784
Rehcramon <reh@cra.mon>
2022-02-08 20:12:41 +0800

    Update version to 0.6.

--- MasterDuelCardTranslator/MDCT_Common.py
+++ MasterDuelCardTranslator/MDCT_Common.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = '0.5'
+VERSION = '0.6'
 
 SHORT_TITLE = 'MDCT v{}'.format(VERSION)
 
